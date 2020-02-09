9927a5b0496859652b0c88759a1d709d3e0ca547
Ahel11 <ahmadelmir7@hotmail.com>
2020-02-09 11:47:41 +0100

    Changed to right import for onclicks

--- src/gui/GuiController.java
+++ src/gui/GuiController.java
@@ -1,11 +1,11 @@
 package gui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-import java.awt.event.ActionEvent;
 
 public class GuiController {
 
